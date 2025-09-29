bdff61e9c391ed27994c66cb446738e803721f35
william_tyler <tyler.william.buscato@gmail.com>
2025-09-29 21:54:32 +0200

    Add files via upload

---
+++ bonzibuddy renewed/src/install dependencies/install dependencies.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:home
+echo Welcome to the bonzibuddy renewed dependencies installation setup!
+echo.
+echo Select the actions below:
+echo.
+echo (1) install dependencies 
+echo.
+
+set /p option=Enter option number:
+if "%option%"=="1" goto 1
+if "%option%"=="2" goto 2
+
+:1
+cls
+pause
+exit
