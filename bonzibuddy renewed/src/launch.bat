f5c90192e1e3c84246c89f9afb5f140329265d01
william_tyler <tyler.william.buscato@gmail.com>
2025-11-13 11:34:16 +0100

    Add files via upload

---
+++ bonzibuddy renewed/src/launch.bat
@@ -0,0 +1,21 @@
+@echo off
+
+findstr /i "yes" "%appdata%\bonzibuddy renewed\firstrun.txt" >nul && goto yes
+findstr /i "no" "%appdata%\bonzibuddy renewed\firstrun.txt" >nul && goto no
+
+:yes
+IF EXIST "C:\Windows\SysWOW64\wscript.exe" (
+start "" "C:\Windows\SysWOW64\wscript.exe" introduction.vbs
+) ELSE (
+start "" introduction.vbs
+)
+echo no > "%appdata%\bonzibuddy renewed\firstrun.txt"
+exit
+
+:no
+IF EXIST "C:\Windows\SysWOW64\wscript.exe" (
+start "" "C:\Windows\SysWOW64\wscript.exe" main.vbs
+) ELSE (
+start "" main.vbs
+)
+exit
